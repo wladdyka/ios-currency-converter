594701539f2c44b5c9a82023f47c28f619d35140
Vlad M <wladdyka@gmail.com>
2024-06-18 20:17:15 +0500

    Initial Commit

---
+++ currency-converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  currency-converter
+//
+//  Created by Vlad M on 18.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ currency-converter/currency_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converterApp.swift
+//  currency-converter
+//
+//  Created by Vlad M on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct currency_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
